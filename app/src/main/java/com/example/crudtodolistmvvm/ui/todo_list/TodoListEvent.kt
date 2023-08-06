0c74bd3c7b5e8ce5648d0988bfe3b89387d40833
fnilvuwu <mamatmks45@gmail.com>
2023-08-06 22:35:44 +0800

    upload project

---
+++ app/src/main/java/com/example/crudtodolistmvvm/ui/todo_list/TodoListEvent.kt
@@ -0,0 +1,11 @@
+package com.example.crudtodolistmvvm.ui.todo_list
+
+import com.example.crudtodolistmvvm.data.Todo
+
+sealed class TodoListEvent {
+    data class OnDeleteTodoClick(val todo: Todo): TodoListEvent()
+    data class OnDoneChange(val todo: Todo, val isDone: Boolean): TodoListEvent()
+    object OnUndoDeleteClick: TodoListEvent()
+    data class OnTodoClick(val todo: Todo): TodoListEvent()
+    object OnAddTodoClick: TodoListEvent()
+}
---
+++ app/src/main/java/com/example/crudtodolistmvvm/util/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.crudtodolistmvvm.util
+
+object Routes {
+    const val TODO_LIST = "todo_list"
+    const val ADD_EDIT_TODO = "add_edit_todo"
+}
